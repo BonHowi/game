1e3f2cd47f5e5e01daa66fc25103e36aa124bc6a
bon <52290046+BonHowi@users.noreply.github.com>
2021-09-25 16:07:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import pygame
 successes, failures = pygame.init()
 print(f"Initializing pygame: {successes} successes and {failures} failures.")
 
-FPS = 120
+FPS = 60
 blit_objects = []  # table of objects to blit
-SIZE = (720, 720)
+SIZE = (1200, 800)
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -21,8 +21,8 @@ myfont = pygame.font.SysFont('Comic Sans MS', 15)
 
 
 class Screen(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *groups):
+        super().__init__(*groups)
         self.image = pygame.Surface(SIZE)
         self.image.fill(BLACK)
         self.rect = self.image.get_rect()  # Get rect of some size as 'image'.
@@ -31,8 +31,8 @@ class Screen(pygame.sprite.Sprite):
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *groups):
+        super().__init__(*groups)
         self.image = pygame.Surface((20, 20))
         self.image.fill(BROWN)
         self.image.set_colorkey(WHITE)
@@ -64,9 +64,9 @@ class Player(pygame.sprite.Sprite):
 
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.Surface((20, 20))
+    def __init__(self, *groups):
+        super().__init__(*groups)
+        self.image = pygame.Surface((2, 2))
         self.image.fill(BLUE)
         self.image.set_colorkey(RED)
         self.rect = self.image.get_rect()
@@ -85,27 +85,52 @@ class Enemy(pygame.sprite.Sprite):
     def collision(self, collided):
         if self.rect.colliderect(collided):
             self.image.fill(RED)
-            self.priority = -10
-        else:
-            self.image.fill(BLUE)
+            self.kill()
 
 
-screen = Screen()
-player = Player()
-enemy = Enemy()
+class FPSCounter:
+    def __init__(self, surface, font, cock, color, pos):
+        self.surface = surface
+        self.font = font
+        self.clock = cock
+        self.pos = pos
+        self.color = color
+
+        self.fps_text = self.font.render(str(int(self.clock.get_fps())) + "FPS", False, self.color)
+        self.fps_text_rect = self.fps_text.get_rect(center=(self.pos[0], self.pos[1]))
+
+    def render(self):
+        self.surface.blit(self.fps_text, self.fps_text_rect)
+
+    def update(self):
+        text = f"{self.clock.get_fps():2.0f} FPS"
+        self.fps_text = self.font.render(text, False, self.color)
+        self.fps_text_rect = self.fps_text.get_rect(center=(self.pos[0], self.pos[1]))
 
 
 def blit_all(blit_objects_list):
     blit_objects_list.sort(key=lambda x: x.priority, reverse=False)
-    for blit_object in blit_objects_list:
-        screen.blit(blit_object.image, blit_object.rect)
+    counter = 0
+    for blit_ob in blit_objects_list:
+        screen.blit(blit_ob.image, blit_ob.rect)
+        counter += 1
+    print(counter)
 
+
+all_sprites = pygame.sprite.Group()
+screen = Screen(all_sprites)
+player = Player(all_sprites)
+
+enemy_list = []
+for _ in range(50):
+    enemy_list.append(Enemy(all_sprites))
 
 running = True
 while running:
     screen = pygame.display.set_mode(SIZE)
     clock = pygame.time.Clock()
     screen_rect = screen.get_rect()
+    fps_counter = FPSCounter(screen, myfont, clock, GREEN, (150, 10))
     dt = clock.tick(FPS) / 400  # Returns milliseconds between each call to 'tick'. The convert time to seconds.
     screen.fill(BLACK)  # Fill the screen with background color.
 
@@ -134,19 +159,25 @@ while running:
                 player.attacking = False
 
     player.rect.clamp_ip(screen_rect)
-    player.update()
 
     coordinates = myfont.render(str(player.rect.x) + ', ' + str(player.rect.y), False, (255, 0, 0))
 
-    enemy.move(dt, player)
-    enemy.rect.clamp_ip(screen_rect)
-    enemy.update()
+    for enemy in enemy_list:
+        if enemy.priority > 0:
+            enemy.move(dt, player)
+            enemy.rect.clamp_ip(screen_rect)
 
-    blit_all(blit_objects)
+    all_sprites.update()
+    all_sprites.draw(screen)
     player.render(screen)
-    screen.blit(coordinates, (0, 0))
 
-    pygame.display.update()  # Or pygame.display.flip()
+
+
+
+    screen.blit(coordinates, (0, 0))
+    fps_counter.render()
+    fps_counter.update()
+    pygame.display.flip()
 
 print("Exited the game loop. Game will quit...")
 quit()
